88a2df48db5e5b1a9d45bcaec663782dea1ef815
zekeriyajpg <104718308+zekeriyajpg@users.noreply.github.com>
2024-08-08 10:47:25 +0300

    Update Program.cs

--- Pratik-Inheritance/Program.cs
+++ Pratik-Inheritance/Program.cs
@@ -7,7 +7,8 @@ baseKisi.Soyad = "Kınık";
 baseKisi.Yazdir();
 
 Console.WriteLine("*****************************************************************");
-
+// Create an instance of Ogrenci and set properties
+// Bir Ogrenci nesnesi oluştur ve özelliklerini ayarla
 Ogrenci ogrenci = new Ogrenci
 {
     Ad = "Ahmet",
@@ -15,13 +16,18 @@ Ogrenci ogrenci = new Ogrenci
     Ogrencino = 123456789
 
 };
+// Print student details
+// Öğrenci bilgilerini yazdır
 ogrenci.Ogrencibilgigetir();
 
 Console.WriteLine("*****************************************************************");
-
+// Create an instance of Ogretmen and set properties
+// Bir Ogretmen nesnesi oluştur ve özelliklerini ayarla
 Ogretmen ogretmen = new Ogretmen();
 
 ogretmen.Ad = "Abdullah";
 ogretmen.Soyad = "Paksoy";
 ogretmen.Maas = 45000;
+// Print teacher details
+// Öğretmen bilgilerini yazdır
 ogretmen.Ogretmenbilgigetir();
