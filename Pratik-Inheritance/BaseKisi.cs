ac9058ad386d58f593645cab612782434e90fa32
zekeriyajpg <104718308+zekeriyajpg@users.noreply.github.com>
2024-08-08 10:50:16 +0300

    Update BaseKisi.cs

--- Pratik-Inheritance/BaseKisi.cs
+++ Pratik-Inheritance/BaseKisi.cs
@@ -6,12 +6,17 @@ using System.Threading.Tasks;
 
 namespace Pratik_Inheritance
 {
+    // Base class
+    // Temel sınıf
     public class BaseKisi
     {
+        // Properties
+        // Özellikler
         public string Ad { get; set; }
         public string Soyad { get; set; }
 
-
+        // Method to print name and surname
+        // Ad ve soyad yazdıran meto
         public void Yazdir()
         {
             Console.WriteLine($"Ad: {Ad} Soyad: {Soyad}");
@@ -19,24 +24,40 @@ namespace Pratik_Inheritance
 
         }
     }
+    // Derived class for students
+    // Öğrenciler için türetilmiş sınıf
 
     class Ogrenci : BaseKisi
     {
+        // Additional property for student number
+        // Öğrenci numarası için ek özellik
         public int Ogrencino { get; set; }
+        // Method to print student number, name, and surname
+        // Öğrenci numarası, ad ve soyad yazdıran metot
 
         public void Ogrencibilgigetir()
         {
             Console.WriteLine("Öğrenci Numarası : " +Ogrencino );
+            // Call the base class method to print name and surname
+            // Ad ve soyad yazdırmak için temel sınıf metodunu çağır
             Yazdir();
         }
     }
+    // Derived class for teachers
+    // Öğretmenler için türetilmiş sınıf
 
     class Ogretmen : BaseKisi
     {
+        // Additional property for salary
+        // Maaş bilgisi için ek özellik
         public int Maas { get; set; }
+        // Method to print salary, name, and surname
+        // Maaş bilgisi, ad ve soyad yazdıran metot
         public void Ogretmenbilgigetir()
         {
             Console.WriteLine("Maaş : " + Maas);
+            // Call the base class method to print name and surname
+            // Ad ve soyad yazdırmak için temel sınıf metodunu çağır
             Yazdir() ;
         }
 
