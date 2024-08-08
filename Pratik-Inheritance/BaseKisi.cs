92654eea9eee874a34332e26935085b3d47b5c8c
ZEK <ZEK@DESKTOP-MQI8H1N>
2024-08-08 10:44:58 +0300

    Proje dosyası ekle.

---
+++ Pratik-Inheritance/BaseKisi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik_Inheritance
+{
+    public class BaseKisi
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+
+
+        public void Yazdir()
+        {
+            Console.WriteLine($"Ad: {Ad} Soyad: {Soyad}");
+
+
+        }
+    }
+
+    class Ogrenci : BaseKisi
+    {
+        public int Ogrencino { get; set; }
+
+        public void Ogrencibilgigetir()
+        {
+            Console.WriteLine("Öğrenci Numarası : " +Ogrencino );
+            Yazdir();
+        }
+    }
+
+    class Ogretmen : BaseKisi
+    {
+        public int Maas { get; set; }
+        public void Ogretmenbilgigetir()
+        {
+            Console.WriteLine("Maaş : " + Maas);
+            Yazdir() ;
+        }
+
+    }
+ }
---
+++ Pratik-Inheritance/Program.cs
@@ -0,0 +1,27 @@
+﻿using Pratik_Inheritance;
+
+BaseKisi baseKisi = new BaseKisi();
+
+baseKisi.Ad = "Ali";
+baseKisi.Soyad = "Kınık";
+baseKisi.Yazdir();
+
+Console.WriteLine("*****************************************************************");
+
+Ogrenci ogrenci = new Ogrenci
+{
+    Ad = "Ahmet",
+    Soyad = "Kaya",
+    Ogrencino = 123456789
+
+};
+ogrenci.Ogrencibilgigetir();
+
+Console.WriteLine("*****************************************************************");
+
+Ogretmen ogretmen = new Ogretmen();
+
+ogretmen.Ad = "Abdullah";
+ogretmen.Soyad = "Paksoy";
+ogretmen.Maas = 45000;
+ogretmen.Ogretmenbilgigetir();
